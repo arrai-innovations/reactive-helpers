503cb2c99b519d8c450141d779775aaaabc9ff82
Joel Hillacre <joel@403forbidden.ca>
2022-05-06 13:14:44 -0600

    refactor: add relatedObjectsPropertyName, add hope coded seperation of relatedObjects from base objects, using proxies instead of mutation.

--- use/listRelated.js
+++ use/listRelated.js
@@ -1,6 +1,7 @@
-import { computed, watch } from "vue";
+import { computed, effectScope, reactive, watch } from "vue";
 import { get, isArray, isEmpty, isUndefined } from "lodash";
 import { keyDiff } from "../utils/keyDiff";
+import proxyMerge from "proxy-merge";
 
 export function useListRelateds(instances, args) {
     for (const [key, value] of Object.entries(args)) {
@@ -8,63 +9,105 @@ export function useListRelateds(instances, args) {
     }
 }
 
-export default function useListRelated({ listInstance, relatedObjectsRules }) {
-    listInstance.state.relatedObjectsRules = relatedObjectsRules;
+export default function useListRelated({
+    parentState,
+    relatedObjectsRules,
+    relatedObjectsPropertyName = "relatedObjects", // NOT REACTIVE
+}) {
+    const state = reactive({
+        relatedObjectsRules: relatedObjectsRules,
+        relatedObjectsObjects: {},
+        relatedObjectsEffectScopes: {},
+        objects: {},
+    });
 
-    function relatedObjectsWatch() {
-        if (listInstance.state.relatedObjectsRules === false) {
-            return;
+    // don't change relatedObjectsPropertyName on us or it will break
+    const ropn = relatedObjectsPropertyName + "";
+
+    function parentStateObjectsWatch() {
+        const { addedKeys, removedKeys } = keyDiff(state.relatedObjectsObjects, parentState.objects);
+        for (const removedKey of removedKeys) {
+            delete state.relatedObjectsObjects[removedKey];
+            delete state.objects[removedKey];
+            if (state.relatedObjectsEffectScopes[removedKey]) {
+                state.relatedObjectsEffectScopes[removedKey].stop();
+                delete state.relatedObjectsEffectScopes[removedKey];
+            }
         }
-        const relatedObjectsIsEmpty = isEmpty(listInstance.state.relatedObjectsRules);
-        for (const object of Object.values(listInstance.state.objects)) {
-            if (!object.relatedObjects) {
-                object.relatedObjects = {};
-            }
-            let removedKeys, addedKeys;
-            if (!relatedObjectsIsEmpty) {
-                ({ removedKeys, addedKeys } = keyDiff(
-                    Object.keys(listInstance.state.relatedObjectsRules),
-                    Object.keys(object.relatedObjects)
-                ));
-            } else {
-                if (isEmpty(object.relatedObjects)) {
-                    continue;
-                }
-                removedKeys = Object.keys(object.relatedObjects);
-                addedKeys = [];
-            }
-            for (const removedKey of removedKeys) {
-                delete object.relatedObjects[removedKey];
-            }
-            for (const addedKey of addedKeys) {
-                object.relatedObjects[addedKey] = computed(() => {
-                    const ro = listInstance.state.relatedObjectsRules?.[addedKey]?.objects;
-                    const key = listInstance.state.relatedObjectsRules?.[addedKey]?.pkKey || addedKey;
-                    if (!ro || !key) {
-                        return undefined;
-                    }
-                    const value = get(object, key);
-                    if (isUndefined(value)) {
-                        return undefined;
-                    }
-                    if (isArray(value)) {
-                        return value.map((e) => ro[e]);
-                    }
-                    return ro[value];
-                });
-            }
+        for (const addedKey of addedKeys) {
+            state.relatedObjectsObjects[addedKey] = {};
+            state.objects[addedKey] = proxyMerge(parentState.objects[addedKey], state.relatedObjectsObjects[addedKey]);
         }
     }
 
-    watch(
-        [
-            () => Object.keys(listInstance.state.objects),
-            () =>
-                listInstance.state.relatedObjectsRules
-                    ? Object.keys(listInstance.state.relatedObjectsRules)
-                    : listInstance.state.relatedObjectsRules,
-        ],
-        relatedObjectsWatch,
-        { immediate: true }
-    );
+    function relatedObjectsWatch() {
+        if (state.relatedObjectsRules === false) {
+            return;
+        }
+        const relatedObjectsRulesIsEmpty = isEmpty(state.relatedObjectsRules);
+        for (const objectKey of Object.keys(state.relatedObjectsObjects)) {
+            const relatedObjectsEffectScope = effectScope();
+            relatedObjectsEffectScope.run(() => {
+                const relatedObjectsObject = state.relatedObjectsObjects[objectKey];
+                const originalObject = parentState.objects[objectKey];
+                if (!relatedObjectsObject[ropn]) {
+                    relatedObjectsObject[ropn] = {};
+                }
+                let removedRuleKeys, addedRuleKeys;
+                if (!relatedObjectsRulesIsEmpty) {
+                    ({ removedKeys: removedRuleKeys, addedKeys: addedRuleKeys } = keyDiff(
+                        Object.keys(state.relatedObjectsRules),
+                        Object.keys(relatedObjectsObject[ropn])
+                    ));
+                } else {
+                    if (isEmpty(relatedObjectsObject[ropn])) {
+                        return;
+                    }
+                    removedRuleKeys = Object.keys(relatedObjectsObject[ropn]);
+                    addedRuleKeys = [];
+                }
+                for (const removedRuleKey of removedRuleKeys) {
+                    delete relatedObjectsObject[ropn][removedRuleKey];
+                }
+                for (const addedRuleKey of addedRuleKeys) {
+                    relatedObjectsObject[ropn][addedRuleKey] = computed(() => {
+                        const ruleObjects = state.relatedObjectsRules?.[addedRuleKey]?.objects;
+                        const rulePkKey = state.relatedObjectsRules?.[addedRuleKey]?.pkKey || addedRuleKey;
+                        if (!ruleObjects || !rulePkKey) {
+                            return undefined;
+                        }
+                        const value = get(originalObject, rulePkKey);
+                        if (isUndefined(value)) {
+                            return undefined;
+                        }
+                        if (isArray(value)) {
+                            return value.map((e) => ruleObjects[e]);
+                        }
+                        return ruleObjects[value];
+                    });
+                }
+            });
+            state.relatedObjectsEffectScopes[objectKey] = relatedObjectsEffectScope;
+        }
+    }
+
+    const es = effectScope();
+
+    es.run(() => {
+        watch(() => Object.key(parentState.objects), parentStateObjectsWatch, { immediate: true });
+        watch(
+            [
+                () => Object.keys(state.relatedObjectsObjects),
+                () => (state.relatedObjectsRules ? Object.keys(state.relatedObjectsRules) : state.relatedObjectsRules),
+            ],
+            relatedObjectsWatch,
+            { immediate: true }
+        );
+    });
+    return {
+        combinedState: proxyMerge(state, parentState),
+        state,
+        parentState,
+        effectScope: es,
+    };
 }
