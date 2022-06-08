6ffc5aef931d165b13e06280c0d03e467d07a9f2
Joel Hillacre <joel@403forbidden.ca>
2022-06-08 14:17:31 -0600

    fix: paged meta vars case should match standard.

--- use/paginatedListInstance.js
+++ use/paginatedListInstance.js
@@ -3,27 +3,27 @@ import { useListInstance } from "./listInstance";
 export function usePagedListInstance({ crudArgs, defaultListArgs = {}, defaultRetrieveArgs = {} }) {
     const listInstance = useListInstance({ crudArgs, defaultListArgs, defaultRetrieveArgs });
 
-    listInstance.state.total_records = 0;
-    listInstance.state.total_pages = 0;
+    listInstance.state.totalRecords = 0;
+    listInstance.state.totalPages = 0;
     listInstance.state.page = 0;
-    listInstance.state.per_page = 0;
+    listInstance.state.perPage = 0;
 
-    listInstance.pageCallback = (newObjects, { total_records, total_pages, page, per_page }) => {
+    listInstance.pageCallback = (newObjects, { totalRecords, totalPages, page, perPage }) => {
         // display one page at a time, clear the list
         listInstance.clearList();
 
         listInstance.defaultPageCallback(newObjects);
-        if (total_records !== undefined) {
-            listInstance.state.total_records = total_records;
+        if (totalRecords !== undefined) {
+            listInstance.state.totalRecords = totalRecords;
         }
-        if (total_pages !== undefined) {
-            listInstance.state.total_pages = total_pages;
+        if (totalPages !== undefined) {
+            listInstance.state.totalPages = totalPages;
         }
         if (page !== undefined) {
             listInstance.state.page = page;
         }
-        if (per_page !== undefined) {
-            listInstance.state.per_page = per_page;
+        if (perPage !== undefined) {
+            listInstance.state.perPage = perPage;
         }
     };
 
