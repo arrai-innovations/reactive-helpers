d9d6a3eddc9b3634186327d3146b09081476f43c
Joel Hillacre <joel@403forbidden.ca>
2022-06-08 13:38:21 -0600

    feat: store more pagination meta data on a paginated list instance.

--- use/paginatedListInstance.js
+++ use/paginatedListInstance.js
@@ -3,15 +3,27 @@ import { useListInstance } from "./listInstance";
 export function usePagedListInstance({ crudArgs, defaultListArgs = {}, defaultRetrieveArgs = {} }) {
     const listInstance = useListInstance({ crudArgs, defaultListArgs, defaultRetrieveArgs });
 
-    listInstance.state.count = 0;
+    listInstance.state.total_records = 0;
+    listInstance.state.total_pages = 0;
+    listInstance.state.page = 0;
+    listInstance.state.per_page = 0;
 
-    listInstance.pageCallback = (newObjects, count) => {
+    listInstance.pageCallback = (newObjects, { total_records, total_pages, page, per_page }) => {
         // display one page at a time, clear the list
         listInstance.clearList();
 
         listInstance.defaultPageCallback(newObjects);
-        if (count !== undefined) {
-            listInstance.state.count = count;
+        if (total_records !== undefined) {
+            listInstance.state.total_records = total_records;
+        }
+        if (total_pages !== undefined) {
+            listInstance.state.total_pages = total_pages;
+        }
+        if (page !== undefined) {
+            listInstance.state.page = page;
+        }
+        if (per_page !== undefined) {
+            listInstance.state.per_page = per_page;
         }
     };
 
