63b8b90c1712235633ceb3473b6bb77db0353e24
Joel Hillacre <joel@403forbidden.ca>
2023-02-26 21:37:21 -0700

    feat: getFakeId can work with maps.

--- utils/getFakeId.js
+++ utils/getFakeId.js
@@ -1,17 +1,17 @@
-import { isArray, isSet } from "lodash";
+import { isArray, isSet, isMap } from "lodash";
 
-export function getFakeId(arraySetOrObject, key = "id") {
+export function getFakeId(arraySetMapOrObject, key = "id") {
     // sets are assumed to be of ids
     // arrays are assumed to be objects with an property matching the passed key
     // objects are assumed to have keys that are ids
     let fakeId;
     let test;
-    if (isSet(arraySetOrObject)) {
-        test = () => arraySetOrObject.has(fakeId);
-    } else if (isArray(arraySetOrObject)) {
-        test = () => arraySetOrObject.some((item) => item[key] === fakeId);
+    if (isSet(arraySetMapOrObject) || isMap(arraySetMapOrObject)) {
+        test = () => arraySetMapOrObject.has(fakeId);
+    } else if (isArray(arraySetMapOrObject)) {
+        test = () => arraySetMapOrObject.some((item) => item[key] === fakeId);
     } else {
-        test = () => fakeId in arraySetOrObject;
+        test = () => fakeId in arraySetMapOrObject;
     }
     do {
         fakeId = Math.floor(Math.random() * Number.MIN_SAFE_INTEGER);
