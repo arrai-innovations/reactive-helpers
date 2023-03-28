330c16cc0c52e5650290c9cbb4dc807506d33048
Joel Hillacre <joel@403forbidden.ca>
2023-03-28 10:49:51 -0600

    fix: watching values directly does not work for reactive object.

--- use/cancellableIntent.js
+++ use/cancellableIntent.js
@@ -1,5 +1,5 @@
 import { identity, isEqual } from "lodash";
-import { effectScope, onScopeDispose, reactive, readonly, watch } from "vue";
+import { effectScope, nextTick, onScopeDispose, reactive, readonly, watch } from "vue";
 
 /*
  * Calls your awaitable function with the arguments you pass in, when the watch arguments change and are all truthy.
@@ -39,49 +39,49 @@ export function useCancellableIntent({ awaitableWithCancel, watchArguments = {},
         return false;
     }
 
-    es.run(() => {
-        watch(
-            Object.values(watchArguments),
-            () => {
-                let newWatchValues = Object.values(watchArguments);
-                if (isEqual(newWatchValues, previousWatchValues)) {
-                    return;
-                }
-                previousWatchValues = newWatchValues;
-                cancel().catch(console.error);
-                if (Object.values(previousWatchValues).every(identity)) {
-                    state.errored = false;
-                    state.error = null;
-                    let awaitablePromise = awaitableWithCancel();
-                    state.active = true;
-                    if (awaitablePromise.cancel) {
-                        cancelFunction = async () => {
-                            state.active = false;
-                            cancelFunction = null;
-                            return awaitablePromise.cancel();
-                        };
-                    }
-                    awaitablePromise
-                        .then(() => {
-                            if (state.clearActiveOnResolved) {
-                                cancelFunction = null;
-                                state.active = false;
-                            }
-                        })
-                        .catch(async (err) => {
-                            await cancel();
-                            console.error(err);
-                            state.errored = true;
-                            state.error = err;
-                            throw err;
-                        });
-                }
-            },
-            {
-                // this can't be immediate because subscribe wants to look at our state, which won't exist yet.
-                deep: true,
+    const watchFn = () => {
+        let newWatchValues = Object.values(watchArguments);
+        if (isEqual(newWatchValues, previousWatchValues)) {
+            return;
+        }
+        previousWatchValues = newWatchValues;
+        cancel().catch(console.error);
+        if (Object.values(previousWatchValues).every(identity)) {
+            state.errored = false;
+            state.error = null;
+            let awaitablePromise = awaitableWithCancel();
+            state.active = true;
+            if (awaitablePromise.cancel) {
+                cancelFunction = async () => {
+                    state.active = false;
+                    cancelFunction = null;
+                    return awaitablePromise.cancel();
+                };
             }
-        );
+            awaitablePromise
+                .then(() => {
+                    if (state.clearActiveOnResolved) {
+                        cancelFunction = null;
+                        state.active = false;
+                    }
+                })
+                .catch(async (err) => {
+                    await cancel();
+                    console.error(err);
+                    state.errored = true;
+                    state.error = err;
+                    throw err;
+                });
+        }
+    };
+
+    es.run(() => {
+        watch(() => Object.values(watchArguments), watchFn, {
+            // this can't be immediate because subscribe wants to look at our state, which won't exist yet.
+            deep: true,
+        });
+
+        nextTick().then(watchFn);
 
         onScopeDispose(cancel);
     });
