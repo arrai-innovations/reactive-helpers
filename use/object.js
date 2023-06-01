961ed71e79eea762bc36490baa00d9b4e7aaa606
Joel Hillacre <joel@403forbidden.ca>
2023-06-01 14:56:59 -0600

    feat: proxy managed object* methods unto returned useObject.

--- use/object.js
+++ use/object.js
@@ -2,7 +2,7 @@ import { useObjectCalculated } from "./objectCalculated";
 import { useObjectInstance } from "./objectInstance";
 import { useObjectRelated } from "./objectRelated";
 import { useObjectSubscription } from "./objectSubscription";
-import { reactive, shallowReadonly, toRef } from "vue";
+import { effectScope, reactive, shallowReadonly, toRef } from "vue";
 
 // Manages a chain of useObject* functions
 export const useObject = ({ props, functions }) => {
@@ -13,27 +13,45 @@ export const useObject = ({ props, functions }) => {
         objectCalculated: null,
     });
 
-    managed.objectInstance = useObjectInstance({
-        props,
-        functions,
+    const es = effectScope();
+
+    es.run(() => {
+        managed.objectInstance = useObjectInstance({
+            props,
+            functions,
+        });
+        managed.objectSubscription = useObjectSubscription({
+            objectInstance: managed.objectInstance,
+            props: reactive({
+                intendToSubscribe: toRef(props, "intendToSubscribe"),
+                intendToRetrieve: toRef(props, "intendToRetrieve"),
+            }),
+        });
+        managed.objectRelated = useObjectRelated({
+            parentState: managed.objectSubscription.state,
+            relatedObjectRules: toRef(props, "relatedObjectRules"),
+        });
+        managed.objectCalculated = useObjectCalculated({
+            parentState: managed.objectRelated.state,
+            calculatedObjectRules: toRef(props, "calculatedObjectRules"),
+        });
     });
-    managed.objectSubscription = useObjectSubscription({
-        objectInstance: managed.objectInstance,
-        props: reactive({
-            intendToSubscribe: toRef(props, "intendToSubscribe"),
-            intendToRetrieve: toRef(props, "intendToRetrieve"),
-        }),
-    });
-    managed.objectRelated = useObjectRelated({
-        parentState: managed.objectSubscription.state,
-        relatedObjectRules: toRef(props, "relatedObjectRules"),
-    });
-    managed.objectCalculated = useObjectCalculated({
-        parentState: managed.objectRelated.state,
-        calculatedObjectRules: toRef(props, "calculatedObjectRules"),
-    });
+    const clearError = () => {
+        managed.objectSubscription.clearError();
+        managed.objectInstance.clearError();
+    };
     return reactive({
         managed: shallowReadonly(managed),
         state: managed.objectCalculated.state,
+        retrieve: managed.objectInstance.retrieve,
+        create: managed.objectInstance.create,
+        update: managed.objectInstance.update,
+        patch: managed.objectInstance.patch,
+        subscribe: managed.objectSubscription.subscribe,
+        unsubscribe: managed.objectSubscription.unsubscribe,
+        updateFromSubscription: managed.objectSubscription.updateFromSubscription,
+        deleteFromSubscription: managed.objectSubscription.deleteFromSubscription,
+        clearError,
+        effectScope: es,
     });
 };
