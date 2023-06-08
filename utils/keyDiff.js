76092f58e064cb862b4a7b06d3ee29e0ebf2432e
Joel Hillacre <joel@403forbidden.ca>
2023-06-08 13:02:35 -0600

    feat: add keyDiffDeep, add more jsdoc comments.

--- utils/keyDiff.js
+++ utils/keyDiff.js
@@ -1,5 +1,29 @@
+import { flattenPaths } from "./flattenPaths.js";
 import { difference, intersection } from "./set.js";
 
+/**
+ * @typedef {object} KeyDiffOptions
+ * @property {boolean} [sameKeys=true]
+ * @property {boolean} [removedKeys=true]
+ * @property {boolean} [addedKeys=true]
+ */
+
+/**
+ * @typedef {object} KeyDiffResult
+ * @property {string[]} [sameKeys]
+ * @property {string[]} [removedKeys]
+ * @property {string[]} [addedKeys]
+ */
+
+/**
+ * Calculate the difference between two arrays of keys, in terms of what keys
+ *  are the same, what keys are removed, and what keys are added.
+ *
+ * @param {string[]} newKeys
+ * @param {string[]} oldKeys
+ * @param {KeyDiffOptions} [options]
+ * @returns {KeyDiffResult}
+ */
 export function keyDiff(newKeys, oldKeys, { sameKeys = true, removedKeys = true, addedKeys = true } = {}) {
     const newKeysSet = new Set(newKeys);
     const oldKeysSet = new Set(oldKeys);
@@ -14,4 +38,20 @@ export function keyDiff(newKeys, oldKeys, { sameKeys = true, removedKeys = true,
         returnValue.addedKeys = difference(newKeysSet, oldKeysSet);
     }
     return returnValue;
+}
+
+/**
+ * Calculate the difference between two objects, in terms of what keys are the same,
+ * what keys are removed, and what keys are added. Keys are sourced deeply in the objects.
+ *
+ * @param {object} newObj
+ * @param {object} oldObj
+ * @param {KeyDiffOptions} [options]
+ * @returns {KeyDiffResult}
+ */
+export function keyDiffDeep(newObj, oldObj, options = {}) {
+    const newPaths = flattenPaths(newObj);
+    const oldPaths = flattenPaths(oldObj);
+    const result = keyDiff(newPaths, oldPaths, options);
+    return result;
 }
