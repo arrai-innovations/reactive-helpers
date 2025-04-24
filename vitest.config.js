25d7f26a7db672c40123e4e54ae43bc127269f4d
Joel Hillacre <joel@403forbidden.ca>
2025-04-24 00:56:17 -0600

    chore: add coverage exclude list

--- vitest.config.js
+++ vitest.config.js
@@ -7,6 +7,15 @@ export default defineConfig({
         environment: "jsdom",
         coverage: {
             reporter: ["text", "json-summary", "html"],
+            exclude: [
+                "**/*.config.js",
+                ".prettierrc.cjs",
+                "check_type_doc.js",
+                "make_type_doc.js",
+                "tests",
+                "typedoc-local-plugins",
+                "types",
+            ],
         },
     },
 });
