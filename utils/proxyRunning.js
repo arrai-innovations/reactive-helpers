067ed90077f2d30cc0ed8d0b7652803cf124922d
Joel Hillacre <joel@403forbidden.ca>
2025-05-21 11:09:22 -0600

    fix: make proxyRunning respond to true immediately via flush: "sync"

--- utils/proxyRunning.js
+++ utils/proxyRunning.js
@@ -5,16 +5,16 @@ import { nextTick, toRef, watch } from "vue";
  *
  * @param {import("vue").UnwrapNestedRefs<object>} parentState - The parent state.
  * @param {string} parentStateProp - The parent state property.
- * @param {import("vue").Ref<boolean>} ref - The ref to proxy to.
+ * @param {import("vue").Ref<boolean>} childRef - The ref to proxy to.
  */
-export function proxyRunning(parentState, parentStateProp, ref) {
+export function proxyRunning(parentState, parentStateProp, childRef) {
     // we want you to have been running false for two ticks before we stop you, but we want you to start running
     //  true immediately the point of this is to reduce running flicker where the stack has running states awaiting
     //  other running states
     let falseTickCount = 0;
     const checkStillFalse = () => {
         if (falseTickCount > 1) {
-            ref.value = false;
+            childRef.value = false;
             falseTickCount = 0;
         } else if (parentState[parentStateProp] === false) {
             falseTickCount++;
@@ -23,12 +23,18 @@ export function proxyRunning(parentState, parentStateProp, ref) {
             falseTickCount = 0;
         }
     };
-    watch(toRef(parentState, parentStateProp), (running) => {
-        if (running && ref.value !== running) {
-            ref.value = running;
-        } else if (!running && ref.value !== running) {
-            nextTick(checkStillFalse);
+    watch(
+        toRef(parentState, parentStateProp),
+        (running) => {
+            if (running && childRef.value !== running) {
+                childRef.value = running;
+            } else if (!running && childRef.value !== running) {
+                nextTick(checkStillFalse);
+            }
+        },
+        {
+            flush: "sync",
         }
-    });
-    ref.value = parentState[parentStateProp];
+    );
+    childRef.value = parentState[parentStateProp];
 }
