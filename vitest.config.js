bf39be235e3ec09030531d2e5c92bf840c476ffe
Joel Hillacre <joel@403forbidden.ca>
2025-05-24 12:09:36 -0600

    test: switch to istanbul for coverage accuracy

--- vitest.config.js
+++ vitest.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         globals: true,
         environment: "jsdom",
         coverage: {
-            provider: "v8",
+            provider: "istanbul",
             reporter: ["text", "json-summary", "html"],
             include: ["config/**/*.js", "use/**/*.js", "utils/**/*.js"],
             exclude: [
